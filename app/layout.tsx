dd506b8a26247362f62435811ebde9e57a9a0c38
budicuy <personal.budicuy@gmail.com>
2026-06-04 09:13:20 +0800

    fix: add overflow-x-hidden class to RootLayout for improved layout handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth overflow-x-hidden`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
